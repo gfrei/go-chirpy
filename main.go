3a8c59c42147ab539ec4287aeeeb236e366b4461
Guilherme Freire <gfreire1992@gmail.com>
2025-09-08 14:36:00 -0300

    feat: add initial server logic to main.go

--- main.go
+++ main.go
@@ -1,1 +1,13 @@
 package main
+
+import "net/http"
+
+func main() {
+	serverMux := http.NewServeMux()
+	server := http.Server{}
+
+	server.Handler = serverMux
+	server.Addr = ":8080"
+
+	server.ListenAndServe()
+}
