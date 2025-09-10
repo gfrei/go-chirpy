5ad226ef654ce8f0e87ba841474035fc981185c4
Guilherme Freire <gfreire1992@gmail.com>
2025-09-10 19:52:12 -0300

    feat: add new handlers

--- main.go
+++ main.go
@@ -1,19 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	handlerFunc := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, req)
+	})
+
+	return handlerFunc
+}
+
+func (cfg *apiConfig) fileserverHitsCountHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) fileserverHitsResetHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+	w.Write([]byte(fmt.Sprintf("Reset Hits: %v", cfg.fileserverHits.Load())))
+}
+
 func main() {
+	apiCfg := &apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-
+	fsHandlerWithMetrics := apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", fsHandlerWithMetrics)
 	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.fileserverHitsCountHandler)
+	mux.HandleFunc("/reset", apiCfg.fileserverHitsResetHandler)
 
-	server := http.Server{}
-	server.Handler = mux
-	server.Addr = ":8080"
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
 
 	log.Fatal(server.ListenAndServe())
 }
