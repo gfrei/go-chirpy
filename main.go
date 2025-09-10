086d794c39d750712b9915817b0f140137b0d311
Guilherme Freire <gfreire1992@gmail.com>
2025-09-10 16:46:26 -0300

    feat: add healthz path

--- main.go
+++ main.go
@@ -7,11 +7,19 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	server := http.Server{}
 	server.Handler = mux
 	server.Addr = ":8080"
 
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
 }
