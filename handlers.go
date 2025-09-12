e42d2f7950223934fc040cbbe7623502ebbdbf7b
Guilherme Freire <gfreire1992@gmail.com>
2025-09-12 17:17:29 -0300

    feat: update metrics to return html

--- handlers.go
+++ handlers.go
@@ -6,7 +6,7 @@ import (
 )
 
 func readinessHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
 }
@@ -14,7 +14,12 @@ func readinessHandler(w http.ResponseWriter, req *http.Request) {
 func (cfg *apiConfig) fileserverHitsCountHandler(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
-	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+	w.Write([]byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())))
 }
 
 func (cfg *apiConfig) fileserverHitsResetHandler(w http.ResponseWriter, req *http.Request) {
